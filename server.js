d9516b555b5f193dbe37b3c86fc04007212d2139
ryo <ba068082@gmail.com>
2018-09-17 10:24:30 +0900

    fix: retry

--- server.js
+++ server.js
@@ -25,10 +25,11 @@
 // // 動いてるかどうかconsoleで見る
 // console.log("Server is up!!");
 
+
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 app.listen(port);
